6f368edba807a925e34ad7fd50af025a7a2dfc1b
ButItDoesRun <90749391+ButItDoesRun@users.noreply.github.com>
2023-03-25 18:49:14 +0100

    -dropdown implemented

--- app/src/main/java/com/example/portfoliop2p_api/DataSelectionActivity.java
+++ app/src/main/java/com/example/portfoliop2p_api/DataSelectionActivity.java
@@ -3,6 +3,12 @@ package com.example.portfoliop2p_api;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
 
 import com.example.portfoliop2p_api.node.Node;
 import com.example.portfoliop2p_api.node.NodeSingleton;
@@ -16,6 +22,14 @@ public class DataSelectionActivity extends AppCompatActivity {
     NodeSingleton nodeSingleton = NodeSingleton.getInstance();
     private Node node;
 
+    //textview
+    AutoCompleteTextView autoCompleteTextView;
+
+    ArrayAdapter<String> adapterItems;
+
+    //key
+    String key;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +40,33 @@ public class DataSelectionActivity extends AppCompatActivity {
 
         //get keys from data storage
         List<String> keys = node.getDataStorageKeys();
+
+        //buttons
+        autoCompleteTextView = findViewById(R.id.auto_complete_txt);
+
+
+        //convert keys to resource list for widget (dropdown menu)
+        adapterItems = new ArrayAdapter<String>(this, R.layout.list_item, keys);
+
+        //set dropdown menu values
+        autoCompleteTextView.setAdapter(adapterItems);
+
+
+        //dropdown menu listener
+        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //get clicked key
+                key = adapterView.getItemAtPosition(i).toString();
+
+                //Give notification that key has been selected
+                Toast.makeText(DataSelectionActivity.this, "Key: " + key, Toast.LENGTH_SHORT).show();
+            }
+
+
+        });
+
 
     }
 }
