ae049a9db5b3560efcfb04d0617677794fece983
ButItDoesRun <90749391+ButItDoesRun@users.noreply.github.com>
2023-03-25 15:59:53 +0100

    -DataSelectionActivity added

---
+++ app/src/main/java/com/example/portfoliop2p_api/DataSelectionActivity.java
@@ -0,0 +1,14 @@
+package com.example.portfoliop2p_api;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DataSelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_selection);
+    }
+}
